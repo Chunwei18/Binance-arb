80ad7f46d000b2ebbee60cbb098a12a605599021
Chunwei18 <124109019+Chunwei18@users.noreply.github.com>
2024-01-20 03:52:30 +0000

    added margin trading system (due to binance restriction for borrowing funds, this strategy is buggy and currently outdated)

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+API_KEY = ""
+API_SECRET= ""
+
+TRADE_SYMBOL = 'RLCUSDT'
+MAX_TRADE_AMOUNT_USD = 40 
+GRID_SIZE_PERCENT = 2.5  # Grid size as a percentage
+GRID_BOUND_PERCENT = 10  # Sets the grid bounds to -20% and 20%
+# Sleep interval in seconds
+SLEEP_INTERVAL = 0.1
+DECIMALS = 0  # Set to 0 for integer values
+
+
